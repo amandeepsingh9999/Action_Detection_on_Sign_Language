ac7a4c062cc603fe8ff17730e709200b32643ae7
amandeepsingh9999 <125952177+amandeepsingh9999@users.noreply.github.com>
2024-04-26 09:30:08 +0530

    Update 3_📝_Testing.py

--- pages/3_📝_Testing.py
+++ pages/3_📝_Testing.py
@@ -1,1 +1,64 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+import streamlit as st
 
+folderView = st.empty()
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+classifier = Classifier("pages/Model/keras_model.h5", "pages/Model/labels.txt")
+
+offset = 20
+imgSize = 300
+folder = "Data/C"
+counter = 0
+labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+          "W", "X", "Y", "Z", "hello", "thankyou"]
+
+while cap.isOpened():
+    success, img = cap.read()
+    # imgOutput = img.copy()
+    hands, img = detector.findHands(img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    if hands:
+
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+        imgCropShape = imgCrop.shape
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            print(prediction, index)
+
+        else:
+
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+        cv2.rectangle(img, (x - offset, y - offset - 50), (x - offset + 90, y - offset - 50 + 50), (255, 0, 255),
+                      cv2.FILLED)
+        cv2.putText(img, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+        cv2.rectangle(img, (x - offset, y - offset), (x + w + offset, y + h + offset), (255, 0, 255), 4)
+        # folderView.image(imgCrop)
+
+    # cv2.imshow("Image",img)
+    folderView.image(img)
+    cv2.waitKey(1)
